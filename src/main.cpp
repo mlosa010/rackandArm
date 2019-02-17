21e73652ccf23a633ed2bb1ba04f10faf7b4dc97
mlosa010 <mlosa010@fiu.edu>
2019-02-17 04:28:21 -0500

    this is one that makes it to stage 4 but the rack does not stop moving and does not progress to stage 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,19 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
+/* Sweep through space
+the following is a state machine that cycles thorugh the enumerated states to pickup and put down a block
+or sphere for sorting in the IEEE SEcon2019 confernece
 */
 #include <Arduino.h>
 #include <Servo.h>
-
+#define elapesdTime 1000
 Servo Rackservo;
 Servo Handservo;
 Servo Baseservo;
-volatile bool I0 = 1;
-volatile bool I1 = 1;
-volatile bool I2 = 1;
-volatile bool I3 = 0;
+float startInterval = 0;
 
 enum graberStates {initial, baseOut, RackDown, handOpen, handClosed, rackUp, baseIn, stop};
 graberStates grabber= initial;
+
+
 
 void grabbingRoutine(void){
 switch(grabber){
@@ -26,101 +21,62 @@ switch(grabber){
     grabber = baseOut;
     break;
   case baseOut:
-    if(bit_is_set(PORTB, PB5))
-    grabber=baseOut;
-
-    if(bit_is_clear(PORTB, PB5)){
-    grabber=RackDown;
-  }else
-  grabber=stop;
-
-  break;
-
-  case RackDown:
-  if(bit_is_set(PORTD, PD5))
-  grabber= RackDown;
-
-  if(bit_is_clear(PORTD, PD5)){
-  grabber=handClosed;
-}else
-grabber=stop;
-  break;
-
-  case handClosed:
-
-  if(bit_is_set(PORTD, PD6))
-  grabber=rackUp;
-  break;
-/*
-  case rackUp:
-
-  if(!I0&&!I1&&I2)
-  grabber=rackUp;
-
-  if(!I0&&I1&&!I2)
-  grabber=baseIn;
-
-  break;
-
-  case baseIn:
-
-  if(I0&&I1&&!I2)
-  grabber=baseIn;
-
-  if(!I0&&I1&&!I2)
-  grabber=baseIn;
-
-  if(I3)
-  grabber=handOpen;
-
-  break;
-
-  case handOpen:
-
-  if(!A3&&A0&&A1&&!A2)
-  grabber=stop;
-
-  break;
-
-*/
-  default:
-  grabber = stop;
-}
-switch (grabber) {
-  case baseOut:
+  do{
   Baseservo.write(0);
   Rackservo.write(90);
   Handservo.write(90);
+}while(bit_is_set(PINC, PC1));
+grabber=RackDown;
   break;
 
   case RackDown:
+  do{
   Baseservo.write(90);
   Rackservo.write(180);
   Handservo.write(90);
+}while(bit_is_set(PIND, PD3));
+grabber=handClosed;
   break;
 
   case handClosed:
-  Baseservo.write(90);
-  Rackservo.write(90);
-  Handservo.write(0);
-  break;
-
-  case rackUp:
-  Baseservo.write(90);
-  Rackservo.write(0);
-  Handservo.write(90);
-  break;
-
-  case baseIn:
-  Baseservo.write(180);
-  Rackservo.write(90);
-  Handservo.write(90);
-  break;
-
-  case handOpen:
+  startInterval=millis();
+  if(millis()-startInterval>=elapesdTime){
+    grabber=rackUp;
+  }else{
   Baseservo.write(90);
   Rackservo.write(90);
   Handservo.write(180);
+}
+break;
+
+  case rackUp:
+  do{
+  Baseservo.write(90);
+  Rackservo.write(0);
+  Handservo.write(90);
+}while(bit_is_set(PIND, PD2));
+grabber=baseIn;
+
+break;
+
+  case baseIn:
+  do{
+  Baseservo.write(180);
+  Rackservo.write(90);
+  Handservo.write(90);
+}while(bit_is_set(PINC, PC0));
+grabber=handOpen;
+  break;
+
+  case handOpen:
+  startInterval=millis();
+  if(millis()-startInterval>=elapesdTime){
+  grabber=stop;
+  }else{
+  Baseservo.write(90);
+  Rackservo.write(90);
+  Handservo.write(0);
+}
 
   case stop:
   Baseservo.write(90);
@@ -138,30 +94,44 @@ switch (grabber) {
 
 }
 int main(void) {
-  Serial.begin(9600);              //  setup serial
-  Rackservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  Handservo.attach(11);
-  Baseservo.attach(10);
-  DDRD &= ~(1<<PD3);
-  PORTD |= (1<<PD3);
-pinMode(2,INPUT_PULLUP);
- DDRD |= (1<<PD4)|(1<<PD5)|(1<<PD6);
- DDRB |= (1<<PB5);
- grabber = initial;
+ Serial.begin(9600);              //  setup serial
+ Rackservo.attach(11);  // attaches the servo on pin 9 to the servo object
+ Handservo.attach(9);
+ Baseservo.attach(10);
+
+ //this sets a timer interrupt in timer 2 independent of timer 1 which is on
+ //pins 9,10 so that i can have a forth servo
+ TCCR2A =0;
+ TCCR2B &= ~(1<<CS20);
+ TCCR2B |= ((1<<CS21)|(1<<CS21));
+ TCNT2 = 0;
+ OCR2A = 125;
+ OCR2B = 125;
+ TIMSK2 |=(1<<OCIE2A)|(1<<OCIE2B);
+
+
+//sets internal pullup to D3, and D2 for using in
+//bit is set in SM
+ DDRD &= ~((1<<PD2)|(1<<PD3));
+ PORTD |= ((1<<PD2)|(1<<PD3));
+ //sets internal pullup to A0, and A1 for using in
+ //bit is set in SM
  DDRC &= ~((1<< PC0)|(1<<PC1));
  PORTC |= ((1 << PC0)|(1<<PC1));
- //attachInterrupt(0, pin2, FALLING);
- EIMSK |= ((1<<INT1)|(1<<INT0));
- EICRA &= ~((1<<ISC10)|(1<<ISC00));
- EICRA |= ((1<<ISC11)|(1<<ISC01));
+
+ //old code for more interrupts
+ //DDRB |= (1<<PB5)|(1<<PB4);
+ // DDRD |= (1<<PD5)|(1<<PD6);
+ //EIMSK |= ((1<<INT1)|(1<<INT0));
+ //EICRA &= ~((1<<ISC10)|(1<<ISC00));
+ //EICRA |= ((1<<ISC11)|(1<<ISC01));
 
  // setting up pin A0,A1 to be used as a PCINTERRUPT
-  PCMSK1 |= ((1<<PCINT8)|(1<<PC1));
+ //for some reason i cant check the bit_is_set in the state machine
+ //without this bit
+  PCMSK1 |= (1<<PC1);
   PCICR |= (1<<PCIE1);
   sei();
-
-
-
 while(1) {
 grabbingRoutine();
 Serial.println(grabber);
@@ -169,20 +139,28 @@ Serial.println(grabber);
 return(0);
 }
 
+//old code with interrupts
+/*
 ISR(PCINT1_vect){
-  //if(bit_is_clear(PORTC, PC0))
-  //PORTD ^= (1<<PD4);//A0=!A0;
-
-//  if(bit_is_clear(PORTC, PC1))
+  Bflag=!Bflag;
   PORTB ^= (1<<PB5);
-  I0=!I0;
-
 }
 ISR(INT0_vect){
-  PORTD ^= (1<<PD5);
-  I1=!I1;
+  Tflag=!Tflag;
+  PORTD ^= (1<<PD6);
 }
 ISR(INT1_vect){
-  I2=!I2;
-  PORTD ^= (1<<PD6);
+  Hflag=!Hflag;
+PORTD ^= (1<<PD5);
+}
+*/
+
+// psudo PWM generation for adding another motor
+ISR(TIMER2_COMPA_vect){
+  TCNT2 = 0;
+  PORTB ^= (1<<PB5);
+}
+ISR(TIMER2_COMPB_vect){
+  TCNT2 = 0;
+PORTB ^= (1<<PB4);
 }

86da10fd1d1b12d97048aa82b39f9081d6f67182
mlosa010 <mlosa010@fiu.edu>
2019-02-23 22:52:00 -0500

    working grab and dump with serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,250 @@
-/* Sweep through space
-the following is a state machine that cycles thorugh the enumerated states to pickup and put down a block
-or sphere for sorting in the IEEE SEcon2019 confernece
-*/
 #include <Arduino.h>
 #include <Servo.h>
-#define elapesdTime 1000
 Servo Rackservo;
 Servo Handservo;
-Servo Baseservo;
-float startInterval = 0;
+Servo BaseservoL;
+Servo BaseservoR;
+Servo Dumpservo;
+int objDumped=0;
+char k;
+bool fflag = false;
+bool dflag = false;
+unsigned long startInterval = 0;
+unsigned long elapesdTime = 2000;
+unsigned long time = 0;
 
-enum graberStates {initial, baseOut, RackDown, handOpen, handClosed, rackUp, baseIn, stop};
-graberStates grabber= initial;
+enum graberStates {initial, baseOut, RackDown, handOpen, handClosed, rackUp, baseIn,stop};
+graberStates grabber = initial;
 
+enum dumpingStates {Inital, BaseOut, HandOpen,BaseIn,pushObj,HandClosed,Stopp};
+dumpingStates dumper = Inital;
+void grabbingRoutine() {
+  switch (grabber) {
+    case initial:
+    Serial.begin(9600);
+      grabber = baseOut;
+      break;
+    case baseOut:
+      do {
+        BaseservoR.write(180);
+        BaseservoL.write(0);//move forward
+        Rackservo.write(90);//still
+        Handservo.write(90);//still
+      } while (bit_is_set(PINC, PC1));
+      grabber = RackDown;
+      break;
 
+    case RackDown:
+      do {
+        PORTD |= (1<<PD4);//digitalWrite(4,HIGH);
+        BaseservoR.write(90);
+        BaseservoL.write(90);//still
+        Rackservo.write(180);//rack down
+        Handservo.write(90);//still
+      } while (bit_is_set(PIND, PD3));
+      startInterval = millis();
+      grabber = handClosed;
+      break;
 
-void grabbingRoutine(void){
-switch(grabber){
-  case initial:
-    grabber = baseOut;
+    case handClosed:
+      do {
+        PORTD &= ~(1<<PD4);
+        BaseservoL.write(90);
+        BaseservoR.write(90);
+        Rackservo.write(90);
+        Handservo.write(0);
+      } while (millis() - startInterval < elapesdTime);
+      grabber = rackUp;
+      break;
+    case rackUp:
+      do {
+        PORTD |= (1<<PD4);
+        BaseservoL.write(90);
+        BaseservoR.write(90);//still
+        Rackservo.write(0);//rack up
+        Handservo.write(90);//still
+      } while (bit_is_set(PIND, PD2));
+      grabber = baseIn;
+
+      break;
+
+    case baseIn:
+      do {
+        PORTD &= ~(1<<PD4);
+        BaseservoR.write(0);
+        BaseservoL.write(180);
+        Rackservo.write(90);
+        Handservo.write(90);
+      } while (bit_is_set(PINC, PC0));
+      grabber = handOpen;
+      break;
+
+    case handOpen:
+      startInterval = millis();
+      do {
+        BaseservoR.write(90);
+        BaseservoL.write(90);
+        Rackservo.write(90);
+        Handservo.write(180);
+      }while(millis() - startInterval < elapesdTime);
+      grabber = stop;
+      break;
+
+    case stop:
+      BaseservoR.write(90);
+      BaseservoL.write(90);
+      Rackservo.write(90);
+      Handservo.write(90);
+      fflag = true;
+      break;
+
+    default:
+      BaseservoR.write(90);
+      BaseservoL.write(90);
+      Rackservo.write(90);
+      Handservo.write(90);
+      break;
+
+  }
+}
+
+void takeADump(){
+switch (dumper) {
+  case Inital:
+  Serial.begin(9600);
+   dumper=BaseOut;
+   break;
+
+  case BaseOut:
+  do {
+    BaseservoR.write(180);
+    BaseservoL.write(0);//move forward
+    Rackservo.write(90);//still
+    Handservo.write(90);//still
+  } while (bit_is_set(PINC, PC1));
+  dumper = HandOpen;
+  break;
+
+  case HandOpen:
+  startInterval = millis();
+  do {
+    BaseservoR.write(90);
+    BaseservoL.write(90);
+    Rackservo.write(90);
+    Handservo.write(180);
+  }while(millis() - startInterval < elapesdTime);
+  objDumped++;
+  dumper = BaseIn;
+  break;
+
+  case BaseIn:
+  do {
+    PORTD &= ~(1<<PD4);
+    BaseservoR.write(0);
+    BaseservoL.write(180);
+    Rackservo.write(90);
+    Handservo.write(90);
+  } while (bit_is_set(PINC, PC0));
+  if(objDumped==3){
+    dumper = Stopp;
+  }else
+  dumper = pushObj;
+  break;
+
+  case pushObj:
+  startInterval = millis();
+  do{
+    Dumpservo.write(0);
+    BaseservoR.write(90);
+    BaseservoL.write(90);
+    Rackservo.write(90);
+    Handservo.write(90);
+  }while(millis() - startInterval < elapesdTime);
+  dumper = HandClosed;
+  break;
+
+  case HandClosed:
+    do {
+      PORTD &= ~(1<<PD4);
+      Dumpservo.write(180);
+      BaseservoL.write(90);
+      BaseservoR.write(90);
+      Rackservo.write(90);
+      Handservo.write(0);
+    } while (millis() - startInterval < elapesdTime);
+    dumper = BaseOut;
     break;
-  case baseOut:
-  do{
-  Baseservo.write(0);
-  Rackservo.write(90);
-  Handservo.write(90);
-}while(bit_is_set(PINC, PC1));
-grabber=RackDown;
-  break;
 
-  case RackDown:
-  do{
-  Baseservo.write(90);
-  Rackservo.write(180);
-  Handservo.write(90);
-}while(bit_is_set(PIND, PD3));
-grabber=handClosed;
-  break;
+    case Stopp:
+      Dumpservo.write(90);
+      BaseservoR.write(90);
+      BaseservoL.write(90);
+      Rackservo.write(90);
+      Handservo.write(90);
+      dflag = true;
+      break;
 
-  case handClosed:
-  startInterval=millis();
-  if(millis()-startInterval>=elapesdTime){
-    grabber=rackUp;
-  }else if(millis()-startInterval<elapesdTime){
-  Baseservo.write(90);
-  Rackservo.write(90);
-  Handservo.write(180);
+      default:
+        Dumpservo.write(90);
+        BaseservoR.write(90);
+        BaseservoL.write(90);
+        Rackservo.write(90);
+        Handservo.write(90);
+        break;
 }
-break;
-
-  case rackUp:
-  do{
-  Baseservo.write(90);
-  Rackservo.write(0);
-  Handservo.write(90);
-}while(bit_is_set(PIND, PD2));
-grabber=baseIn;
-
-break;
-
-  case baseIn:
-  do{
-  Baseservo.write(180);
-  Rackservo.write(90);
-  Handservo.write(90);
-}while(bit_is_set(PINC, PC0));
-grabber=handOpen;
-  break;
-
-  case handOpen:
-  startInterval=millis();
-  if(millis()-startInterval>=elapesdTime){
-  grabber=stop;
-  }else{
-  Baseservo.write(90);
-  Rackservo.write(90);
-  Handservo.write(0);
 }
 
-  case stop:
-  Baseservo.write(90);
-  Rackservo.write(90);
-  Handservo.write(90);
-  break;
 
-  default:
-  Baseservo.write(90);
-  Rackservo.write(90);
-  Handservo.write(90);
-  break;
 
+void setup() {
+  Serial.begin(9600);              //  setup serial
+  Rackservo.attach(11);  // attaches the servo on pin 9 to the servo object
+  Handservo.attach(9);
+  BaseservoL.attach(10);
+  BaseservoR.attach(6);
+  Dumpservo.attach(5);
+
+    DDRD &= ~((1<<PD2)|(1<<PD3));//pinMode(2,3INPUTS);
+    DDRD |= (1<<PD4);
+    PORTD &= ~(1<<PD4);
+    PORTD |= ((1<<PD2)|(1<<PD3));
+    DDRC &= ~((1<< PC0)|(1<<PC1));
+    PORTC |= ((1 << PC0)|(1<<PC1));
+}
+void resetState()
+{
+  grabber = initial;
+  dumper = Inital;
+  fflag=false;
+  dflag=false;
+  k='i';
+  //break();
 }
 
-}
-int main(void) {
- Serial.begin(9600);              //  setup serial
- Rackservo.attach(11);  // attaches the servo on pin 9 to the servo object
- Handservo.attach(9);
- Baseservo.attach(10);
+void loop() {
+  Serial.println(grabber);
+  Serial.print(dumper);
 
- //this sets a timer interrupt in timer 2 independent of timer 1 which is on
- //pins 9,10 so that i can have a forth servo
- TCCR2A =0;
- TCCR2B &= ~(1<<CS20);
- TCCR2B |= ((1<<CS21)|(1<<CS21));
- TCNT2 = 0;
- OCR2A = 125;
- OCR2B = 125;
- TIMSK2 |=(1<<OCIE2A)|(1<<OCIE2B);
+  if(Serial.available()){
+    k=Serial.read();
+    Serial.end();
+  }
+  if(fflag || dflag)
+  {
+    resetState();
+  }
+  if(k == 'p'){
+    //Serial.end();
+    //grabber=initial;
+    grabbingRoutine();
 
 
-//sets internal pullup to D3, and D2 for using in
-//bit is set in SM
- DDRD &= ~((1<<PD2)|(1<<PD3));
- PORTD |= ((1<<PD2)|(1<<PD3));
- //sets internal pullup to A0, and A1 for using in
- //bit is set in SM
- DDRC &= ~((1<< PC0)|(1<<PC1));
- PORTC |= ((1 << PC0)|(1<<PC1));
+  }
+  if(k=='d'){
 
- //old code for more interrupts
- //DDRB |= (1<<PB5)|(1<<PB4);
- // DDRD |= (1<<PD5)|(1<<PD6);
- //EIMSK |= ((1<<INT1)|(1<<INT0));
- //EICRA &= ~((1<<ISC10)|(1<<ISC00));
- //EICRA |= ((1<<ISC11)|(1<<ISC01));
-
- // setting up pin A0,A1 to be used as a PCINTERRUPT
- //for some reason i cant check the bit_is_set in the state machine
- //without this bit
-  PCMSK1 |= (1<<PC1);
-  PCICR |= (1<<PCIE1);
-  sei();
-while(1) {
-grabbingRoutine();
-Serial.println(grabber);
-}
-return(0);
+    takeADump();
+  }
 }
 
-//old code with interrupts
-/*
-ISR(PCINT1_vect){
-  Bflag=!Bflag;
-  PORTB ^= (1<<PB5);
-}
-ISR(INT0_vect){
-  Tflag=!Tflag;
-  PORTD ^= (1<<PD6);
-}
-ISR(INT1_vect){
-  Hflag=!Hflag;
-PORTD ^= (1<<PD5);
-}
-*/
 
-// psudo PWM generation for adding another motor
-ISR(TIMER2_COMPA_vect){
-  TCNT2 = 0;
-  PORTB ^= (1<<PB5);
-}
-ISR(TIMER2_COMPB_vect){
-  TCNT2 = 0;
-PORTB ^= (1<<PB4);
-}
+//////////////////////define routines////////////////////////

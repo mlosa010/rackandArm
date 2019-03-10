7c6dca481a41031e6f6cc5875d5c12c5475212a6
mlosa010 <mlosa010@fiu.edu>
2019-03-09 23:37:09 -0500

    base servos rotating in the right direction, rack servo still not there, cleaner code with inlines for grabbing

--- src/main.cpp
+++ src/main.cpp
@@ -12,98 +12,162 @@ bool dflag = false;
 unsigned long startInterval = 0;
 unsigned long elapesdTime = 2000;
 unsigned long time = 0;
+int speed=0;
 
 enum graberStates {initial, baseOut, RackDown, handOpen, handClosed, rackUp, baseIn,stop};
 graberStates grabber = initial;
 
 enum dumpingStates {Inital, BaseOut, HandOpen,BaseIn,pushObj,HandClosed,Stopp};
 dumpingStates dumper = Inital;
+
+inline void BASEOUTE();
+inline void RACKDOWN();
+inline void HANDCLOSED();
+inline void RACKUP();
+inline void BASEIN();
+inline void HANDOPEN();
+inline void STOP();
+void grabbingRoutine();
+void takeADump();
+
+void setup() {
+  Serial.begin(9600);              //  setup serial
+  Rackservo.attach(11,500,2400);  // attaches the servo on pin 9 to the servo object
+  Handservo.attach(9);
+  BaseservoL.attach(10);
+  Dumpservo.attach(5);
+  Rackservo.writeMicroseconds(600);
+
+    DDRD &= ~((1<<PD2)|(1<<PD3));
+    DDRD |= ((1<<PD4)|(1<<PD5));
+    PORTD &= ~((1<<PD4)|(1<<PD5));
+    PORTD |= ((1<<PD2)|(1<<PD3));
+    DDRC &= ~((1<< PC0)|(1<<PC1));
+    PORTC |= ((1 << PC0)|(1<<PC1));
+}
+void resetState()
+{
+  grabber = initial;
+  dumper = Inital;
+  fflag=false;
+  dflag=false;
+  k='i';
+}
+
+void loop() {
+  Serial.println(grabber);
+  Serial.print(dumper);
+
+  if(Serial.available()){
+    k=Serial.read();
+    Serial.end();
+  }
+  if(fflag || dflag)
+    resetState();
+
+  if(k == 'p')
+    grabbingRoutine();
+
+  if(k=='d')
+    takeADump();
+}
+
+/////////////////////define inline functions used in rutines//////////
+//for those who dont know inline functions are simple code repacements like #defines
+//but they look like functions
+inline void BASEOUTE(){
+  do {
+    BaseservoL.write(180);
+    Rackservo.writeMicroseconds(600);
+    Handservo.write(90);
+  } while (bit_is_set(PINC, PC1));
+  grabber = RackDown;
+}
+inline void RACKDOWN(){
+  do {
+    PORTD |= (1<<PD4);
+    BaseservoL.write(90);
+    Rackservo.writeMicroseconds(2400);
+    Handservo.write(90);
+  } while (bit_is_set(PIND, PD3));
+  startInterval = millis();
+  PORTD &= ~(1<<PD4);
+  grabber = handClosed;
+}
+inline void HANDCLOSED(){
+  do {
+    BaseservoL.write(90);
+    Rackservo.writeMicroseconds(600);
+    Handservo.write(0);
+  } while (millis() - startInterval < elapesdTime);
+  grabber = rackUp;
+}
+inline void RACKUP(){
+  do {
+    PORTD |= (1<<PD4);
+    BaseservoL.write(90);
+    Rackservo.writeMicroseconds(0);//rack up
+    Handservo.write(90);//still
+  } while (bit_is_set(PIND, PD2));
+  PORTD &= ~(1<<PD4);
+  grabber = baseIn;
+}
+inline void BASEIN(){
+  PORTD &= ~(1<<PD4);
+  do {
+    BaseservoL.write(0);
+    Rackservo.writeMicroseconds(600);
+    Handservo.write(90);
+  } while (bit_is_set(PINC, PC0));
+  grabber = handOpen;
+}
+inline void HANDOPEN(){
+  startInterval = millis();
+  do {
+    BaseservoL.write(90);
+    Rackservo.writeMicroseconds(600);
+    Handservo.write(180);
+  }while(millis() - startInterval < elapesdTime);
+  grabber = stop;
+}
+inline void STOP(){
+  BaseservoL.write(90);
+  Rackservo.writeMicroseconds(600);
+  Handservo.write(90);
+  fflag = true;
+}
+
+
+//////////////////////define routines////////////////////////
 void grabbingRoutine() {
   switch (grabber) {
     case initial:
     Serial.begin(9600);
-      grabber = baseOut;
-      break;
+    grabber = baseOut;
+    break;
     case baseOut:
-      do {
-        BaseservoR.write(180);
-        BaseservoL.write(0);//move forward
-        Rackservo.write(90);//still
-        Handservo.write(90);//still
-      } while (bit_is_set(PINC, PC1));
-      grabber = RackDown;
+      BASEOUTE();
       break;
-
     case RackDown:
-      do {
-        PORTD |= (1<<PD4);//digitalWrite(4,HIGH);
-        BaseservoR.write(90);
-        BaseservoL.write(90);//still
-        Rackservo.write(0);//rack down
-        Handservo.write(90);//still
-      } while (bit_is_clear(PIND, PD3));
-      startInterval = millis();
-      PORTD &= ~(1<<PD4);
-      grabber = handClosed;
+      RACKDOWN();
       break;
-
     case handClosed:
-      do {
-
-        BaseservoL.write(90);
-        BaseservoR.write(90);
-        Rackservo.write(90);
-        Handservo.write(0);
-      } while (millis() - startInterval < elapesdTime);
-      grabber = rackUp;
+      HANDCLOSED();
       break;
     case rackUp:
-      do {
-        PORTD |= (1<<PD4);
-        BaseservoL.write(90);
-        BaseservoR.write(90);//still
-        Rackservo.write(180);//rack up
-        Handservo.write(90);//still
-      } while (bit_is_clear(PIND, PD2));
-      grabber = baseIn;
-
+      RACKUP();
       break;
-
     case baseIn:
-      do {
-        PORTD &= ~(1<<PD4);
-        BaseservoR.write(0);
-        BaseservoL.write(180);
-        Rackservo.write(90);
-        Handservo.write(90);
-      } while (bit_is_set(PINC, PC0));
-      grabber = handOpen;
+      BASEIN();
       break;
-
     case handOpen:
-      startInterval = millis();
-      do {
-        BaseservoR.write(90);
-        BaseservoL.write(90);
-        Rackservo.write(90);
-        Handservo.write(180);
-      }while(millis() - startInterval < elapesdTime);
-      grabber = stop;
+      HANDOPEN();
       break;
-
     case stop:
-      BaseservoR.write(90);
-      BaseservoL.write(90);
-      Rackservo.write(90);
-      Handservo.write(90);
-      fflag = true;
+      STOP();
       break;
-
     default:
-      BaseservoR.write(90);
-      BaseservoL.write(90);
-      Rackservo.write(90);
-      Handservo.write(90);
+      STOP();
       break;
 
   }
@@ -194,58 +258,3 @@ switch (dumper) {
         break;
 }
 }
-
-
-
-void setup() {
-  Serial.begin(9600);              //  setup serial
-  Rackservo.attach(11);  // attaches the servo on pin 9 to the servo object
-  Handservo.attach(9);
-  BaseservoL.attach(10);
-  BaseservoR.attach(6);
-  Dumpservo.attach(5);
-
-    DDRD &= ~((1<<PD2)|(1<<PD3));//pinMode(2,3INPUTS);
-    DDRD |= (1<<PD4);
-    PORTD &= ~(1<<PD4);
-    PORTD |= ((1<<PD2)|(1<<PD3));
-    DDRC &= ~((1<< PC0)|(1<<PC1));
-    PORTC |= ((1 << PC0)|(1<<PC1));
-}
-void resetState()
-{
-  grabber = initial;
-  dumper = Inital;
-  fflag=false;
-  dflag=false;
-  k='i';
-  //break();
-}
-
-void loop() {
-  Serial.println(grabber);
-  Serial.print(dumper);
-
-  if(Serial.available()){
-    k=Serial.read();
-    Serial.end();
-  }
-  if(fflag || dflag)
-  {
-    resetState();
-  }
-  if(k == 'p'){
-    //Serial.end();
-    //grabber=initial;
-    grabbingRoutine();
-
-
-  }
-  if(k=='d'){
-
-    takeADump();
-  }
-}
-
-
-//////////////////////define routines////////////////////////
